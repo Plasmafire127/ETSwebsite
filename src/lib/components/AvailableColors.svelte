5fca69b5d9810a3b557c5135a9189f10a8b94a68
Jake <jdigiug1@binghamton.edu>
2025-02-05 16:41:40 -0500

    finished colors

--- src/lib/components/AvailableColors.svelte
+++ src/lib/components/AvailableColors.svelte
@@ -2,24 +2,38 @@
     const colors = 
     [
     ['#000000','Black'],
+    ['#616378','Grey'],
     ['#9e9c95','Silver'],
     ['#FFFFFF','White'],
+    ['#F3C79F','Tan'],
+    ['#573A28','Brown'],
+    ['#E20460','Magenta'],
+    ['#E82201','Red'],
+    ['#FF9B0F','Orange'],
+    ['#E1B304','Gold'],
+    ['#F9E504','Yellow'],
+    ['#6CCB33','Lime Green'],
+    ['#8C9856','Olive Green'],
+    ['#62CAE3','Light Blue'],
+    ['#003CC9','Royal Blue'],
+    ['#00006B','Blue'],
+    ['#6103A0','Purple'],
+    ['#FF6DA1','Pink'],
     ];
 </script>
 
 
-<div class="font-plex-mono text-white flex flex-col">
-    <p class="flex items-end md:text-5xl text-2xl font-bold md:px-20 px-5 w-full md:pt-0 pt-10 custom-height-md">
-        Available Colors
-    </p>
-    <div class="flex items-center justify-center pt-5">
-        <div class="grid grid-cols-6 grid-rows-4">
-            {#each colors as [color,name]}
-            <div>
-                <div class="w-32 h-32 mx-4 mt-4 mb-2 rounded-lg border-white border-2" style="background-color: {color};"></div>
-                <div class="text-center font-bold italic">{name}</div>
-            </div>
-            {/each}
+<div class="font-plex-mono text-white flex flex-col md:px-20">
+    <div class="flex flex-col items-start md:text-5xl text-2xl font-bold px-5 w-full pt-10 custom-height-md">
+        <p>Available Colors</p>
+        <p class="text-xl">(PLA!)</p>
+    </div>
+    <div class="flex flex-row flex-wrap items-center justify-center pt-5">
+        {#each colors as [color,name]}
+        <div>
+            <div class="md:w-32 w-16 md:h-32 h-16 md:mx-8 mx-4 mt-4 mb-2 rounded-lg border-white border-2" style="background-color: {color};"></div>
+            <div class="text-center font-bold italic md:text-md text-xs">{name}</div>
         </div>
+        {/each}
     </div>
 </div>
