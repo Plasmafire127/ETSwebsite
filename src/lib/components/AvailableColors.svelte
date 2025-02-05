2b95d13fb94ac4e6ac9d0dc72a162a112f183abb
unknown <ETSBing@ETS-Desktop1.bu.binghamton.edu>
2025-02-05 14:09:26 -0500

    added colors scaffolding

---
+++ src/lib/components/AvailableColors.svelte
@@ -0,0 +1,25 @@
+<script>
+    const colors = 
+    [
+    ['#000000','Black'],
+    ['#9e9c95','Silver'],
+    ['#FFFFFF','White'],
+    ];
+</script>
+
+
+<div class="font-plex-mono text-white flex flex-col">
+    <p class="flex items-end md:text-5xl text-2xl font-bold md:px-20 px-5 w-full md:pt-0 pt-10 custom-height-md">
+        Available Colors
+    </p>
+    <div class="flex items-center justify-center pt-5">
+        <div class="grid grid-cols-6 grid-rows-4">
+            {#each colors as [color,name]}
+            <div>
+                <div class="w-32 h-32 mx-4 mt-4 mb-2 rounded-lg border-white border-2" style="background-color: {color};"></div>
+                <div class="text-center font-bold italic">{name}</div>
+            </div>
+            {/each}
+        </div>
+    </div>
+</div>
